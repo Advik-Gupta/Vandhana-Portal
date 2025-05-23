552a09a2fa4c0f2d1648c2396e8178925be13d81
advikgupta <advikgupta@gmail.com>
2025-05-24 01:43:59 +0530

    created database models and basic express router

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./config/db.js";
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000" }));
@@ -12,9 +16,15 @@ const port = process.env.PORT || 8080;
 
 // ______________________________________________________________________________________________
 
+import machineRoutes from "./routes/machineRoutes.js";
+
+// ______________________________________________________________________________________________
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/machines", machineRoutes);
 
 // ______________________________________________________________________________________________
 
