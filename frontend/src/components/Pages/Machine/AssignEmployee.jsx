feb0b14eb16bf078c89e55b7769c650c4f4a16db
deep17raj <deeprajkumarwrs@gmail.com>
2025-06-07 12:41:27 +0530

    aaded search functionality in add machine

--- frontend/src/components/Pages/Machine/AssignEmployee.jsx
+++ frontend/src/components/Pages/Machine/AssignEmployee.jsx
@@ -7,24 +7,28 @@ import { fetchEmployees } from "../../api/machine";
 function AssignEmployee({ onSelectEngineer }) {
   const [assignedEmployee, setAssignedEmployee] = useState(null);
   const [employees, setEmployees] = useState([]);
+  const [searchEmp, setSearchEmp] = useState("")
 
   useEffect(() => {
     const getEmployees = async () => {
       const data = await fetchEmployees();
-      setEmployees(data);
+      const filtered = data.filter(u => u.name.toLowerCase().includes(searchEmp));
+      setEmployees(filtered);
       console.log("Fetched employees:", data);
     };
     getEmployees();
-  }, []);
+  }, [searchEmp]);
 
   const handleAssign = (id) => {
     setAssignedEmployee(id);
   };
-
+  const handleSearchEmp =(val)=>{
+    setSearchEmp(val)
+  }
   return (
     <div className="w-[600px] max-h-[700px] bg-white rounded-2xl border border-gray-200 shadow-xl p-6">
       <div className="mb-4">
-        <SearchBar />
+        <SearchBar onHandleSearch = {handleSearchEmp} />
       </div>
 
       <div className="max-h-[300px] overflow-y-auto pr-2 space-y-3">
